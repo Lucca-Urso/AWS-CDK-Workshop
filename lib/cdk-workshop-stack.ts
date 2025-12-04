b1084b1b0149605e80f4c3679aec55bd6af06a2a
Lucca-Urso <lgursog13@gmail.com>
2025-12-04 10:57:17 -0300

    helloWithCounter Lambda handler impl

---
+++ lib/cdk-workshop-stack.ts
@@ -0,0 +1,27 @@
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Duration, Stack, StackProps } from 'aws-cdk-lib/core';
+import { Construct } from 'constructs';
+import { HitCounter } from './hitcounter';
+
+export class CdkWorkshopStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // AWS Lambda Configuration
+    const hello = new Function(this, "HelloHandler", {
+      runtime: Runtime.NODEJS_22_X, // Execution environment
+      code: Code.fromAsset("lambda"), // Code loaded from "lambda" directory
+      handler: "hello.handler", // File is "hello.js", function name is "handler"
+    });
+
+    const helloWithCounter = new HitCounter(this, "HelloHitCounter", {
+      downstream: hello,
+    });
+
+    // AWS API Gateway Configuration
+    const gateway = new LambdaRestApi(this, "Endpoint", {
+      handler: helloWithCounter.handler,
+    });
+  }
+}
